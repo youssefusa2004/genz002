c7d03b2e6d40810cfb12e582489d45db2297b777
Youssef Mohamed Zakaria <93492531+youssefusa2004@users.noreply.github.com>
2026-01-15 12:09:05 +0200

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+
+export const config = {
+  matcher: '/:path*',
+};
+
+export default async function middleware(req) {
+  const url = req.nextUrl.clone();
+  
+  // الرابط المحجوب
+  const targetHost = 'genz002.net';
+  url.hostname = targetHost;
+  url.protocol = 'https';
+  url.port = '';
+
+  const requestHeaders = new Headers(req.headers);
+  
+  // هذه الأسطر هي الأهم لحل مشكلة "Not accessible"
+  requestHeaders.set('host', targetHost);
+  requestHeaders.set('origin', `https://${targetHost}`);
+  requestHeaders.set('referer', `https://${targetHost}/`);
+  
+  // محاولة إرسال IP المستخدم (لحل مشكلة التصويت)
+  const forwardedFor = req.headers.get('x-forwarded-for');
+  if (forwardedFor) {
+    requestHeaders.set('X-Forwarded-For', forwardedFor);
+  }
+
+  return NextResponse.rewrite(url, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
